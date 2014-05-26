850ff2bb81dce9bb0bdf65ff02adeb339a40163d
Wojciech Izykowski <wizykowski@gmail.com>
2014-05-26 22:49:20 +0200

    more tests

--- tests.c
+++ tests.c
@@ -4,11 +4,14 @@
 
 #include "sprp64.h"
 #include "sprp32.h"
+#include "myrand.h"
 
 int test_modular_inverse64()
 {
 	uint64_t a;
-	for (a=3; a < 500000; a+=2) {
+	int i;
+
+	for (a=3; a < 5000000; a+=2) {
 		uint64_t received = modular_inverse64(a);
 		int64_t received_minus_one = received * a;
 
@@ -18,7 +21,19 @@ int test_modular_inverse64()
 		}
 	}
 
-	for (a = UINT64_MAX; a > UINT64_MAX - 500000; a -= 2) {
+	for (a = UINT64_MAX; a > UINT64_MAX - 5000000; a -= 2) {
+		uint64_t received = modular_inverse64(a);
+		int64_t received_minus_one = received * a;
+
+		if (received_minus_one != -1) {
+			printf("expected: -1, received: %" PRIu64 ", argument: %" PRIu64 "\n", received_minus_one, a);
+			return 1;
+		}
+	}
+
+	myseed();
+	for (i=0; i < 5000000; i++) {
+		a = myrand64() | 1;
 		uint64_t received = modular_inverse64(a);
 		int64_t received_minus_one = received * a;
 
@@ -34,7 +49,9 @@ int test_modular_inverse64()
 int test_modular_inverse32()
 {
 	uint32_t a;
-	for (a = 3; a < 500000; a += 2) {
+	int i;
+
+	for (a = 3; a < 5000000; a += 2) {
 		uint32_t received = modular_inverse32(a);
 		int32_t received_minus_one = received * a;
 
@@ -44,7 +61,19 @@ int test_modular_inverse32()
 		}
 	}
 
-	for (a = UINT32_MAX; a > UINT32_MAX - 500000; a -= 2) {
+	for (a = UINT32_MAX; a > UINT32_MAX - 5000000; a -= 2) {
+		uint32_t received = modular_inverse32(a);
+		int32_t received_minus_one = received * a;
+
+		if (received_minus_one != -1) {
+			printf("expected: -1, received: %" PRIu32 ", argument: %" PRIu32 "\n", received_minus_one, a);
+			return 1;
+		}
+	}
+
+	myseed();
+	for (i=0; i < 5000000; i++) {
+		a = myrand32() | 1;
 		uint32_t received = modular_inverse32(a);
 		int32_t received_minus_one = received * a;
 
